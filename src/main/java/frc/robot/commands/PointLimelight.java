cd271edd47c0b91ff8ec18c4b82cfe5746337ea5
Robot1235369 <73447037+Robot1235369@users.noreply.github.com>
2023-02-17 03:00:07 +0000

    stuff

---
+++ src/main/java/frc/robot/commands/PointLimelight.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Chassis;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.sensors.Limelight;
+
+/** An example command that uses an example subsystem. */
+public class PointLimelight extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Chassis chassis;
+  private final Limelight limelight;
+
+  public PointLimelight(Chassis m_chassis, Limelight m_limelight) {
+    m_chassis = chassis;
+    m_limelight = limelight;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_chassis);
+    addRequirements(m_limelight);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (90 < getHeading() < 270) {
+      m_limelight.led(true);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
