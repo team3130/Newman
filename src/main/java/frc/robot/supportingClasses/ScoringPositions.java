46285292fe114163dd023b3420a1efaedca33454
Caleb <calebkugel1@gmail.com>
2023-03-01 10:13:40 -0600

    made a constants file for scoring positions

---
+++ src/main/java/frc/robot/supportingClasses/ScoringPositions.java
@@ -0,0 +1,23 @@
+package frc.robot.supportingClasses;
+
+public class ScoringPositions {
+    // should be 9 items
+    public final static double[] kBlueAllianceScoringXPositions = {
+
+    };
+
+    // should be  9 items
+    public final static double[] kBlueAllianceScoringYPositions = {
+
+    };
+
+    // should be 9 items
+    public final static double[] kRedAllianceScoringXPositions = {
+
+    };
+
+    // should be 9 items
+    public final static double[] kRedAllianceScoringYPositions = {
+            
+    };
+}
