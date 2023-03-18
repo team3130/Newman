b675d062bba6bb2d8304b74fd1846f045007cd80
Robot1235369 <73447037+Robot1235369@users.noreply.github.com>
2023-03-18 01:27:49 +0000

    I actually fixed it Gavin is lying, I used his computer

--- src/main/java/frc/robot/subsystems/RotaryArm.java
+++ src/main/java/frc/robot/subsystems/RotaryArm.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Newman_Constants.Constants;
 
 /**
- * The subsystem for the rotary arM
+ * The subsystem for the rotary arm
  */
 public class RotaryArm extends SubsystemBase {
   private double outputSpeed = 0.6; // the speed we will run the rotary arm at
