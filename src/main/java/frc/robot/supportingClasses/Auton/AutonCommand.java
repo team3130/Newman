126aef5b901e011d489526ab62f3741e87d35654
Caleb <calebkugel1@gmail.com>
2023-03-29 19:21:50 -0500

    Revert "shallower angle"
    This reverts commit f12ebb88940367dfa3053c69f1b1acd86f694041.

--- src/main/java/frc/robot/supportingClasses/Auton/AutonCommand.java
+++ src/main/java/frc/robot/supportingClasses/Auton/AutonCommand.java
@@ -16,8 +16,6 @@ import frc.robot.commands.Placement.AutoZeroRotryArm;
 import frc.robot.commands.Placement.presets.GoToHighScoring;
 import frc.robot.commands.Placement.presets.GoToLowScoring;
 import frc.robot.commands.Placement.presets.GoToMidScoringCube;
-import frc.robot.commands.Placement.presets.*;
-import frc.robot.commands.TimedCommand;
 import frc.robot.subsystems.*;
 
 import java.util.ArrayList;
@@ -51,7 +49,7 @@ public class AutonCommand extends CommandBase {
     protected final Timer m_timer; // timer from the holonomic drive command
 
     protected final HashMap<EventMarker, Integer> markerToCommandMap; // the marker's mapped to the command to run
-    protected final ArrayList<Integer> indicesToRun = new ArrayList<>(8); // what indices of commands run right now
+    protected final ArrayList<Integer> indicesToRun = new ArrayList<>(5); // what indices of commands run right now
 
     protected final Function<EventMarker, Integer> getOptimizedIndex; // gets the index using the optimize function
 
@@ -100,7 +98,7 @@ public class AutonCommand extends CommandBase {
             m_requirements.addAll(List.of(m_rotaryArm, m_extensionArm, m_manipulator));
         }
 
-        CommandBase HOPPER = null, PLACE_LOW = null, PLACE_MID = null, PLACE_HIGH = null, ZERO = null, PICK_UP, DO_NOTHING = null;
+        CommandBase HOPPER = null, PLACE_LOW = null, PLACE_MID = null, PLACE_HIGH = null, ZERO = null, DO_NOTHING = null;
 
         if (m_hopper != null) {
             HOPPER = new SpinHopper(m_hopper);
@@ -125,12 +123,7 @@ public class AutonCommand extends CommandBase {
 
             ZERO = new SequentialCommandGroup(new AutoZeroExtensionArm(extensionArm), new AutoZeroRotryArm(rotaryArm));
 
-            PICK_UP_OFF_GROUND = new GoToPickupOffGround(m_rotaryArm, m_extensionArm);
-
-            PICK_UP_IN_BOT = new SequentialCommandGroup(new GoToPickupWithinBot(m_extensionArm),
-                    new ToggleManipulator(m_manipulator), new AutoZeroExtensionArm(extensionArm), new AutoZeroRotryArm(rotaryArm));
-
-            CommandScheduler.getInstance().registerComposedCommands(PLACE_LOW, PLACE_MID, PLACE_HIGH, ZERO, PICK_UP_OFF_GROUND, PICK_UP_IN_BOT);
+            CommandScheduler.getInstance().registerComposedCommands(PLACE_LOW, PLACE_MID, PLACE_HIGH, ZERO);
         }
 
         DO_NOTHING = new DoNothing();
