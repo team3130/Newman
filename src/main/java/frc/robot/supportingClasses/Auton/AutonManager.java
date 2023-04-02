e8f72c6faf0fd88b5e333f77a5f91bf9ec8edd6c
Caleb <calebkugel1@gmail.com>
2023-04-02 00:10:31 -0500

    fixed java docs syntax error

--- src/main/java/frc/robot/supportingClasses/Auton/AutonManager.java
+++ src/main/java/frc/robot/supportingClasses/Auton/AutonManager.java
@@ -121,7 +121,7 @@ public class AutonManager {
      *  The second parameter is a {@link Rotation2d} rotation,
      *      the rotation dictates the {@link edu.wpi.first.math.spline.Spline} that is made,
      *      which is basically the rotation of the plane that intersects the wheels when the bot gets to a position
-     *      @see <a href=https://docs.google.com/presentation/d/1Us-ONi37lHcfJIlmSMmEISfco7uoSHt6dLa4VgWFYe8/edit?usp=sharing>
+     *      @see <a href="https://docs.google.com/presentation/d/1Us-ONi37lHcfJIlmSMmEISfco7uoSHt6dLa4VgWFYe8/edit?usp=sharing">
      *          An explanation on holonomic rotation vs heading.
      *          </a>
      *  The third parameter is another {@link Rotation2d} rotation,
@@ -207,8 +207,9 @@ public class AutonManager {
 
     /**
      * Example for how to generate a trajectory and generate a path from a list of {@link PathPoint} object
-     * @see <a href=https://docs.google.com/document/d/1RInEhl8mW1UKMP4AbvWWiWmfI4klbDfyZLJbw1zbjDo/edit#heading=h.lie7pmqbolmu>
-     *     Explanation of PathPoint objects</a>
+     * @see <a href="https://docs.google.com/document/d/1RInEhl8mW1UKMP4AbvWWiWmfI4klbDfyZLJbw1zbjDo/edit#heading=h.lie7pmqbolmu">
+     *     Explanation of PathPoint objects
+     *     </a>
      * @return the command generated
      */
     public CommandBase generateExamplePathFromPoses() {
