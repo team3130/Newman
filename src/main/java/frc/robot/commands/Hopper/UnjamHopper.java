395576aeb74196335a85a520f33a2f751b928713
Juhar <jcsong06@gmail.com>
2023-12-02 15:19:56 -0600

    unjam hopper correction

--- src/main/java/frc/robot/commands/Hopper/UnjamHopper.java
+++ src/main/java/frc/robot/commands/Hopper/UnjamHopper.java
@@ -25,13 +25,12 @@ public class UnjamHopper extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    m_hopper.alternateHopper();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_hopper.alternateHopper();
   }
 
   // Called once the command ends or is interrupted.
